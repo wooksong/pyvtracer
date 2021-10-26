c9778f5d05bb23dbe700a33ee7a62521f8e98107
Wook Song <wook16.song@samsung.com>
2021-10-26 18:40:21 +0900

    dist: Add setup.py that uses the setuptools-rust extension
    To provide the Wheel package, this patch adds setup.py that uses the
    setuptools plugin for Rust extension.
    
    Signed-off-by: Wook Song <wook16.song@samsung.com>

---
+++ pyvtracer/__init__.py
@@ -0,0 +1,1 @@
+from .pyvtracer import *
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import find_packages, setup
+from setuptools.dist import Distribution as _Distribution
+from setuptools_rust import Binding, RustExtension
+
+
+class BinaryDistribution(_Distribution):
+    def has_ext_modules(foo):
+        return True
+
+
+setup(
+    name="pyvtracer",
+    version="0.1.0",
+    author="Wook Song",
+    author_email="wook16.song@samsung.com",
+    maintainer="Wook Song",
+    maintainer_email="wook16.song@samsung.com",
+    rust_extensions=[RustExtension(
+        "pyvtracer.pyvtracer", binding=Binding.PyO3)],
+    packages=find_packages(),
+    zip_safe=False,
+    distclass=BinaryDistribution,
+)
